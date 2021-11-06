8bb7d10c3c0a82a443db95a7a5560655e75d973e
Amberas <amber.aspeslagh@student.howest.be>
2021-11-06 13:23:07 +0100

    upload image #2

---
+++ react + recognition/components/File.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+
+export default function File() {
+    const [image, setImage] = useState(null);
+
+
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        })();
+    }, []);
+
+    const handleUploade = async() => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    }
+
+
+    return(
+            <View>
+                <Button onPress={handleUploade} title="Upload image"/>
+                {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            </View>
+    );
+};
