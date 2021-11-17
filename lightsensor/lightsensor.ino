cc159464538ad37a2457d903e4917dab0ae95c12
Amberas <amber.aspeslagh@student.howest.be>
2021-11-17 13:41:22 +0100

    lightsensor first setup #13

---
+++ lightsensor/lightsensor.ino
@@ -0,0 +1,31 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-light-sensor
+ */
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // reads the input on analog pin A0 (value between 0 and 1023)
+  int analogValue = analogRead(A0);
+
+  Serial.print("Analog reading: ");
+  Serial.print(analogValue);   // the raw analog reading
+
+  // We'll have a few threshholds, qualitatively determined
+  if (analogValue < 5) {
+    Serial.println(" - Shadow");
+  } else if (analogValue < 15) {
+    Serial.println(" - Indirect light");
+  } else if (analogValue > 30) {
+    Serial.println(" - Direct light");
+  }
+
+  delay(500);
+}
