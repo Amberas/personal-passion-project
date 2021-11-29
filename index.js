8d5f5a92716452d20daeb7646f330eb49a231ca9
Amberas <amber.aspeslagh@student.howest.be>
2021-11-29 18:25:48 +0100

    osc + processing function

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const { Client } = require("node-osc");
 
 const client = new Client('192.168.0.174', 8000);
 
+const map_range = (value, low1, high1, low2, high2) => {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
 board.on("ready", () => {
     const sensor = new Sensor("A0");
 
     sensor.on("change", function () {
-        console.log(this.value);
-
-        client.send('/light', this.value);
+        client.send('/light', map_range(this.value, 0, 10, 0, 1));
     });
 });
 
