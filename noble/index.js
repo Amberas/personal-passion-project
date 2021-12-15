4ebe3ddf1b8f3df216aad8deda5666be80391a36
Amberas <amber.aspeslagh@student.howest.be>
2021-12-15 12:45:41 +0100

    write to char

--- noble/index.js
+++ noble/index.js
@@ -19,12 +19,12 @@ const serviceUUIDs = {
 //primairy characteristics UUIDs 
 const characteristicUUIDs = {
     Arduino: [['12345678-1234-5678-1234-56789abcdef1'], ['12345678-1234-5678-1234-56789abcdef2']],
-    ESP32: ['beb5483e-36e1-4688-b7f5-ea07361b26a8']
+    ESP32: [['beb5483e-36e1-4688-b7f5-ea07361b26a8'], ['a1bee35a-5ab9-11ec-bf63-0242ac130002'], ["c92ecec0-5c51-11ec-bf63-0242ac130002"]]
 }
 
 const listen = (variable, name, valueLow, valueHigh) => {
     variable.on('data', function (data, isNotification) {
-        //console.log(`${name}:`, data.readUInt8(0));
+        console.log(`${name}:`, data.readUInt8(0));
         const number = data.readUInt8(0)
         client.send(`/${name}`, map_range(number, valueLow, valueHigh, 0, 1));
     });
@@ -53,7 +53,7 @@ const kalmanFilterFunc = (observations, newObservations) => {
     return newObservations[newObservations.length - 1]
 }
 
-const sendDistance = (rssi, name) => {
+const sendDistance = (rssi, name, char) => {
     console.log("send distance")
     if (rssi) {
         if (name == "Arduino") {
@@ -70,8 +70,23 @@ const sendDistance = (rssi, name) => {
             const newRSSI = kalmanFilterFunc(observationsESP32, newObservationsESP32);
             console.log("newRSSI ESP32:", newRSSI);
             if (newRSSI <= -60) {
+                char.write(Buffer.from('0', 'utf8'), true, function (error) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log("written");
+                    }
+                }.bind(this));    
                 client.send(`/${name}`, 0);
             } else {
+                char.write(Buffer.from('1', 'utf8'), true, function (error) { 
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log("written");
+                    }
+                }.bind(this));    
+
                 client.send(`/${name}`, 1);
             }
 
@@ -113,8 +128,6 @@ noble.on('discover', async (peripheral) => {
             }
             )
         }, 2000);
-        const txPower = peripheral.advertisement.txPower;
-        console.log(txPower);
         const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(serviceUUIDs.Arduino, characteristicUUIDs.Arduino.values());
         const up = await characteristics[0];
         const down = await characteristics[1];
@@ -129,16 +142,20 @@ noble.on('discover', async (peripheral) => {
 
     if (localName == "ESP32") {
         await peripheral.connectAsync();
+        const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(serviceUUIDs.ESP32, characteristicUUIDs.ESP32.values());
+        const soil = await characteristics[0];
+        const touch = await characteristics[1];
+        const screen = await characteristics[2];
         setInterval(function () {
             peripheral.updateRssi(function () {
                 const rssi = peripheral.rssi;
-                sendDistance(rssi, "ESP32");
+                sendDistance(rssi, "ESP32", screen);
             }
             )
-        }, 500);
-        const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(serviceUUIDs.ESP32, characteristicUUIDs.ESP32);
-        const soil = await characteristics[0];
+        }, 2000);
+
         listen(soil, "soil", 10, 250);
+        listen(touch, "touch", 5, 10);
     }
 
 });
